1eb85599d46cd7ff9086aea9faca4b0872558e9c
Souhel <Sudkisouhel@gmail.com>
2022-11-09 15:19:31 +0300

    fix some issu-2

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -21,7 +21,7 @@ const ProductDetails = ({ product, products }) => {
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} alt="product-img" className="product-detail-image" />
+            <img src={urlFor(image && image[index])} alt="product-img" className="product-detail-image" />
           </div>
           <div className="small-images-container">
             {image?.map((item, i) => (
